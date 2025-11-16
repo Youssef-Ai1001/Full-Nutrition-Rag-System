306c8eb2515d1608dd4ca146193cafcd606f3b2c
Youssef-Ai1001 <ytaha8586@gmail.com>
2025-11-16 11:12:12 +0200

    Add docstrings to data model classes and methods for improved documentation and clarity on database operations related to assets, chunks, and projects.

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -5,19 +5,47 @@ from sqlalchemy import func
 
 
 class ProjectModel(BaseDataModel):
+    """
+    This class handles all the database operations related to projects.
+    """
 
     def __init__(self, db_client: object):
+        """
+        Initializes the ProjectModel.
+
+        Args:
+            db_client (object): The database client.
+        """
         super().__init__(db_client=db_client)
         self.db_client = db_client
 
     @classmethod
     async def create_instance(cls, db_client: object):
+        """
+        Creates an instance of the ProjectModel.
+
+        Args:
+            db_client (object): The database client.
+
+        Returns:
+            ProjectModel: An instance of the ProjectModel.
+        """
         instance = cls(db_client)
         return instance
 
     async def create_project(self, project: Project):
+        """
+        Creates a new project in the database.
+
+        Args:
+            project (Project): The project to create.
+
+        Returns:
+            Project: The created project.
+        """
         async with self.db_client() as session:
             async with session.begin():
+                # Add the project to the session
                 session.add(project)
             await session.commit()
             await session.refresh(project)
@@ -25,12 +53,23 @@ class ProjectModel(BaseDataModel):
         return project
 
     async def get_or_create_project(self, project_id: int):
+        """
+        Gets a project from the database or creates a new one if it doesn't exist.
+
+        Args:
+            project_id (int): The ID of the project.
+
+        Returns:
+            Project: The project.
+        """
         async with self.db_client() as session:
             async with session.begin():
+                # Select the project with the given ID
                 query = select(Project).where(Project.id == project_id)
                 result = await session.execute(query)
                 project = result.scalar_one_or_none()
                 if project is None:
+                    # Create a new project if it doesn't exist
                     project_record = Project(id=project_id)
                     project = await self.create_project(project=project_record)
                     return project
@@ -38,8 +77,19 @@ class ProjectModel(BaseDataModel):
                     return project
 
     async def get_all_projects(self, page: int = 1, page_size: int = 10):
+        """
+        Gets all projects from the database with pagination.
+
+        Args:
+            page (int, optional): The page number. Defaults to 1.
+            page_size (int, optional): The number of projects per page. Defaults to 10.
+
+        Returns:
+            A tuple containing a list of projects and the total number of pages.
+        """
         async with self.db_client() as session:
             async with session.begin():
+                # Get the total number of projects
                 total_documents = await session.execute(
                     select(func.count(Project.project_id))
                 ).scalar_one()
@@ -49,6 +99,7 @@ class ProjectModel(BaseDataModel):
                 if total_documents % page_size > 0:
                     total_pages += 1
 
+                # Select all projects with pagination
                 query = select(Project).offset((page - 1) * page_size).limit(page_size)
                 projects = await session.execute(query).scalars().all()
 
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,6 @@
+"""
+This package contains the data models for the application.
+"""
+
 from .enums.ResponseEnums import ResponseSignal
 from .enums.ProcessingEnum import ProcessingEnum
